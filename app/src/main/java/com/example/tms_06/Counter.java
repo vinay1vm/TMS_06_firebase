ddd17cf78f4be7b1942d6d8f267adf9a0cc19907
vinay <vinayvm67@gmail.com>
2022-11-27 13:47:42 +0530

    no use

--- app/src/main/java/com/example/tms_06/Counter.java
+++ app/src/main/java/com/example/tms_06/Counter.java
@@ -6,12 +6,12 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Counter extends ServiceRequest{
     //DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tms-06-default-rtdb.firebaseio.com/");
-     static int id = 0;
+    static int id = 0;
 
     static void Count() {
         id = id + 1;
 
-      //
+        //
 
     }
 
